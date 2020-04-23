34008797a500a4cb061825fa2babafb651154290
greenemax <mjgreene27@gmail.com>
2020-04-23 08:51:38 -0400

    App now pushes X and O when clicking on game board. Working to specify where image appears when user clicks on board next.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,22 +6,25 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 const gameEvents = require('./games/events')
-const clickX = function() {
-  var imgX = document.createElement('img')
-  imgX.src = 'assets/images/xsmall.png';
-  document.getElementById('top-left').appendChild(imgX);
-}
 
-const clickO = function() {
-  var imgO = document.createElement('img')
-  imgO.src = 'assets/images/letterO.png';
-  document.getElementById('top-right').appendChild(imgO);
-}
+let totalClicks = 0
+$(".col-4").click(function() {
+  totalClicks += 1
+  if (totalClicks === 1 ) {
+    let imgX = document.createElement('img')
+    imgX.src = 'assets/images/xsmall.png'
+    document.getElementById('top-left').appendChild(imgX)
+  } else {
+    var imgO = document.createElement('img')
+    imgO.src = 'assets/images/letterO.png'
+    document.getElementById('top-right').appendChild(imgO)
+  }
+})
 
 $(() => {
   $("#top-left").on("click", clickX)
   $("#top-right").on("click", clickO)
-  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-up').on('sub mit', authEvents.onSignUp)
   // $('#sign-up').on('submit', function (event) {
   //   // stuff
   // })
